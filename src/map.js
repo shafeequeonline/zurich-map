8d22d8bd3ee0f60f2ec75a26ba5845934e767bc9
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-14 12:59:17 +0530

    added markers with details

--- src/map.js
+++ src/map.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import GoogleMapReact from 'google-map-react';
 import styles from './styles/main.scss';
+
 // Imported Local JSON Data
 import MapData from './data.json'
 
-const Marker = ({ text }) => <div className={styles.marker}>{text}</div>;
+const Marker = ({ text, tooltip, parking, price }) => { return <div className={styles.marker}>{text} <div className={styles.markerTooltip}>{tooltip} {parking} {price}</div></div>};
 
 const places = MapData;
+console.log(places);
 
 let [...rest] = places;
 
@@ -16,53 +18,52 @@ rest.forEach((place) => {
     let {Coordinates, ...rest} = place
     mapMarkers.push({
         // Coordinates, rest
+        // Removed "POINT", ("(") and (")") from the json data. used chain method to join multiple logics
         // cords: place.Coordinates.split('POINT').pop().split('(').pop().split(')').shift().split(' '),
         // price: place["Price/m^2"],
         // type: place.BuildingType,
         // parking: place.Parking
-        cords:Coordinates.split('POINT').pop().split('(').pop().split(')').shift().split(' '), rest
+
+        // Removed "POINT", ("(") and (")") from the json data. used chain method to join multiple logics
+        // added rest datas to the object 
+        cords:Coordinates.split('POINT').pop().split('(').pop().split(')').shift().split(' '), 
+        price: rest["Price/m^2"],
+        rest
     })
 })
-console.log(mapMarkers);
-rest = rest[0].Coordinates.split('POINT').pop().split('(').pop().split(')').shift().split(' ')
-
-
-const coordinates = places.map((place) => {
-    // Removed "POINT", ("(") and (")") from the json data. used chain method to join multiple logics
-    let cords = place.Coordinates.split('POINT').pop().split('(').pop().split(')').shift().split(' ');
-    return cords
-})
-// console.log(coordinates);
+// console.log(mapMarkers);
 
 class Map extends Component {
-  static defaultProps = {
-    center: {
-      lat: 47.3836514,
-      lng: 8.5482374
-    },
-    zoom: 13
-  };
-/**<Marker
-                        lat={47.3836514}
-                        lng={8.5482374}
-                        text="My Marker"
-                    />**/
-  render() {
-    return (
-        <Fragment>
-            <div style={{ height: '80vh', width: '100%' }}>
-                <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyAuOXhl7_BjeaV30YlT3I9OH6oW9DkYdlA' }}
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
-                >
-                    {mapMarkers.map((mark, index) => <Marker lat={mark.cords[0]} lng={mark.cords[1]} text={index} tooltip={mark.rest.BuildingType}  />)}
-                    
-                </GoogleMapReact>
-            </div>
-      </Fragment>
-    );
-  }
+    static defaultProps = {
+        center: {
+        lat: 47.3836514,
+        lng: 8.5482374
+        },
+        zoom: 13
+    };
+    
+    /**<Marker at={47.3836514} lng={8.5482374} text="My Marker" />
+     * <Marker lat={marker.cords[0]} lng={marker.cords[1]} text={index} tooltip={marker.type} parking={marker.parking} price={marker.price}/>
+     * **/
+    
+    render() {
+        return (
+            <Fragment>
+                <div style={{ height: '80vh', width: '100%' }}>
+                    <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyAuOXhl7_BjeaV30YlT3I9OH6oW9DkYdlA' }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    >
+                        {mapMarkers.map((marker, index) => 
+                            <Marker lat={marker.cords[0]} lng={marker.cords[1]} text={index} tooltip={marker.rest.BuildingType} parking={marker.rest.Parking} price={marker.price}/>
+                        )}
+                        
+                    </GoogleMapReact>
+                </div>
+        </Fragment>
+        );
+    }
 }
 
 export default Map;
